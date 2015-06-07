45b5de1f8afa113c24a4f65383d5e6a48d3df352
huzhiyong <523345150@qq.com>
2015-06-07 05:06:23 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,39 @@
+# source 'http://rubygems.org'
+source 'http://ruby.taobao.org/'
+
+gem 'rails', '~> 4.1.7'
+gem 'mysql2', '~> 0.3.15'
+gem 'turbolinks', '~> 2.2.1'
+gem 'jquery-rails', '~> 3.1.0'
+gem 'jquery-migrate-rails', '~> 1.2.1'
+gem "therubyracer", '~> 0.12.1'
+gem "less-rails", '~> 2.5.0'
+gem "twitter-bootstrap-rails", '~> 2.2.8'
+gem 'ckeditor', '~> 4.0.11'
+# gem 'ckeditor'#, :git => 'https://github.com/abdulsattar/ckeditor'
+gem 'paperclip', '~> 4.1.1'
+gem 'inherited_resources', '~> 1.4.1'
+gem "select2-rails"
+gem 'grape', '~> 0.6.1'
+gem 'newrelic_rpm'
+gem 'pony'
+gem 'liquid'
+# gem 'unicorn'
+gem 'net-ssh'#, '~> 2.9.1', :git => "https://github.com/net-ssh/net-ssh"
+gem 'uglifier'
+gem 'coffee-rails'
+
+group :devlopment do
+  gem "spring-commands-rspec"
+  gem 'spring'
+  gem 'capistrano', '~> 3.1.0'
+  gem 'capistrano-rails', '~> 1.1'
+  # gem 'capistrano-rvm'
+  gem 'capistrano-bundler', '~> 1.1.2'
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0.beta'
+  gem 'factory_girl_rails', '~> 4.4.1'
+  gem 'database_cleaner'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,79 @@
+# config valid only for Capistrano 3.1
+# lock '3.1.0'
+
+set :application, 'backend'
+set :repo_url, 'git@smsgithub.com:demohour/open-sms.git'
+# ask :branch, :platform_mode
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/data/www/open/sms'
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml config/config.yml config/unicorn/production.rb}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+set :default_env, { path: "/usr/local/ruby/bin:$PATH" }
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# def run_remote_rake(rake_cmd)
+#   rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+#
+#   cmd = "cd #{fetch(:latest_release)} && bundle exec #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+#   cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+#   %x|#{cmd}|
+# end
+
+namespace :deploy do
+  after :publishing, :restart
+
+  desc 'Restart application'
+  task :restart do
+    # on roles(:worker), in: :groups, limit: 3, wait: 10 do
+    #   within release_path do
+    #     execute :rake, "db:seed RAILS_ENV=production"
+    #   end
+    # end
+    # invoke 'unicorn:restart'
+  end
+
+
+
+  # after :restart, :restart_workers
+
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+      #
+    # end
+  # end
+
+
+
+
+
+
+
+
+
+end
