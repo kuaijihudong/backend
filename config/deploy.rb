b2731e7a2d7b88d0c166fa597a499fb4b7e65a2b
huzhiyong <523345150@qq.com>
2015-06-08 11:15:09 +0000

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, '/var/www/backend'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/config.yml config/unicorn/production.rb}
+set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml config/unicorn/production.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
