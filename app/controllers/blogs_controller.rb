5795482142bb6104f0c50be2f77905eff7313aa0
huzhiyong <523345150@qq.com>
2015-06-09 11:16:23 +0000

    添加发布新闻行

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,5 +2,20 @@ class BlogsController < ApplicationController
 
   before_action :authenticate_admin!
 
-  
+  def new
+    @blog = Blog.new
+  end
+
+
+  def create
+    
+  end
+
+  def edit
+
+  end
+
+
+  def update
+  end
 end
