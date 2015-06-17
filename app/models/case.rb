797e91822bc3aa68b3d3d728a55160db7840ae79
huzhiyong <523345150@qq.com>
2015-06-17 16:46:19 +0000

    fix

--- app/models/case.rb
+++ app/models/case.rb
@@ -7,5 +7,6 @@ class Case < ActiveRecord::Base
 
   has_attached_file :logo#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
-
+  has_attached_file :images#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
 end
